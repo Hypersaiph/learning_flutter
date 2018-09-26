7725915d8eb3e6df211239a0eaac89f1609acba5
geovani-mendoza-mojix-com <geovani.mendoza@mojix.com>
2018-09-26 14:44:02 -0400

    YOLOOO 2

--- lib/main.dart
+++ lib/main.dart
@@ -90,7 +90,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             new Text(
-              'YOLO:',
+              'YOLO 2:',
             ),
             new Text(
               '$_counter',
